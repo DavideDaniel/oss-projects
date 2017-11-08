e3d2d4e65e8a42270b85414f0f41b4dc8f36dfc5
David Daniel <davidedaniel@gmail.com>
2017-11-08 00:38:05 -0800

    1.0.0

---
+++ packages/node-git-utils/README.md
@@ -0,0 +1,10 @@
+### node-git-utils
+---
+###### v 1.0.0
+
+Thank goodness for git. Just using git and exporting some handy functions for usage in node tools.
+
+#### Install
+```bash
+$ npm install node-git-utils --save-dev
+```
---
+++ packages/node-git-utils/index.js
@@ -0,0 +1,68 @@
+const execa = require('execa');
+
+function execSync(command, args, opts) {
+  return execa.sync(command, args, opts).stdout;
+}
+
+function concatAndFilter(a, b) {
+  return a.concat(b).filter(str => str.length);
+}
+
+function getCurrentSHA(opts) {
+  return execSync('git', ['rev-parse', 'HEAD'], opts);
+}
+
+function getCurrentBranch(opts) {
+  return execSync('git', ['rev-parse', '--abbrev-ref', 'HEAD'], opts);
+}
+
+function getLastTag(opts) {
+  return execSync('git', ['describe', '--tags', '--abbrev=0'], opts);
+}
+
+function getLastTaggedCommit(opts) {
+  return execSync('git', ['rev-list', '--tags', '--max-count=1'], opts);
+}
+
+function getTagsFromCommit(opts) {
+  return execSync('git', ['describe', '--tags', opts.commit], opts);
+}
+
+function getLastTaggedCommitInBranch(opts) {
+  return execSync('git', ['rev-list', '-n', '1', getLastTag(opts)], opts);
+}
+
+function hasTags(opts) {
+  return !!execSync('git', ['tag'], opts);
+}
+
+function getCommitsSinceLastTag(opts) {
+  const options = opts || {};
+  const { folderPath, withMerges } = options;
+  const includeMerges = withMerges ? '' : '--no-merges';
+  const fp = folderPath || '';
+  const args = concatAndFilter(['log', `${getLastTag()}..HEAD`, '--pretty=%s | [%an]'], [includeMerges, fp]);
+  return execSync('git', args, options);
+}
+
+function getInbetweenCommits(opts) {
+  const {
+    a, b, withMerges, folderPath,
+  } = opts;
+  const between = `${a}...${b}`;
+  const includeMerges = withMerges ? '' : '--no-merges';
+  const fp = folderPath || '';
+  const args = concatAndFilter(['log', between, '--pretty=%s | [%an]'], [includeMerges, fp]);
+  return execSync('git', args, opts);
+}
+
+module.exports = {
+  getInbetweenCommits,
+  getCommitsSinceLastTag,
+  getLastTaggedCommitInBranch,
+  getLastTaggedCommit,
+  getCurrentBranch,
+  getCurrentSHA,
+  getTagsFromCommit,
+  hasTags,
+};
