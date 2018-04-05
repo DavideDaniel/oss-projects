b74110fb2d887c0eeb561155159086e3c9ae04cc
David Daniel <davidedaniel@gmail.com>
2018-04-05 04:15:53 -0700

    chore: npm says I have to change the name

--- packages/p-require/index.test.js
+++ packages/p-require/index.test.js
@@ -3,7 +3,7 @@ const pRequire = require('./index');
 describe('pRequire', () => {
   it('should resolve a require when present', async () => {
     const pkg = await pRequire('./package.json');
-    expect(pkg.name).toEqual('p-require');
+    expect(pkg.name).toEqual('require-then');
   });
   it('should reject when module is not available', async () => {
     await pRequire('THROW_ERROR').catch(err => expect(err).toBeTruthy());
