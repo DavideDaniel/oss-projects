65c63ffd65df5830e1b6689f5b6285e58eb4cbdc
David Daniel <davidedaniel@gmail.com>
2018-04-14 19:52:13 -0700

    chore: rm require-then in favor of import-lazy

--- packages/require-then/index.js
+++
@@ -1,8 +0,0 @@
-module.exports = request => new Promise((res, rej) => {
-  try {
-    // eslint-disable-next-line global-require, import/no-dynamic-require
-    res(require(request));
-  } catch (err) {
-    rej(err);
-  }
-});
