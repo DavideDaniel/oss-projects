b8e024f1d0d102efb950821590ea16538a13f00f
David Daniel <davidedaniel@gmail.com>
2021-12-07 00:28:11 -0800

    feat: enable passing in additional args to `lerna ls` and `lerna updated`

--- packages/lerna-utils/2.0/get-updated-packages.js
+++ packages/lerna-utils/2.0/get-updated-packages.js
@@ -1,5 +1,6 @@
 const execa = require('execa');
 const checkStdErr = require('../check-stderr');
+const shapeLernaUpdatedCommand = require('./shape-lerna-updated-command');
 
 const errString = 'No packages need updating';
 const noUpdates = new RegExp(errString);
@@ -18,11 +19,12 @@ function evalExpectedOnly(str) {
  * 2.0/getUpdatedPackages
  * @description this version is specific to lerna 2.0 range and is a way to use lerna's output for updated --json and resolve or reject a list of packages
  * @param {object} [logger=console] any logger to log stdout/stderr
+ * @param {Array} [lernaUpdatedArgs=[]] valid args for `lerna ls` with lerna 2.0
  * @returns {Promise} promise that resolves list of updated packages only
  */
-module.exports = async function getUpdatedPackages(logger = console) {
+module.exports = async function getUpdatedPackages(logger = console, lernaUpdatedArgs = []) {
   try {
-    const { stdout } = await execa.shell('lerna updated --json');
+    const { stdout } = await execa.shell(shapeLernaUpdatedCommand(lernaUpdatedArgs));
 
     return Promise.resolve({
       packages: evalExpectedOnly(stdout),
--- packages/lerna-utils/__tests__/get-updated-packages.test.js
+++ packages/lerna-utils/__tests__/get-updated-packages.test.js
@@ -1,5 +1,7 @@
 const getUpdatedPkgs = require('../2.0/get-updated-packages');
+const shapeLernaUpdatedCommand = require('../2.0/shape-lerna-updated-command');
 const getUpdatedPackages = require('../3.0/get-updated-packages');
+const shapeLernaLsCommand = require('../3.0/shape-lerna-ls-command');
 
 describe('2.0', () => {
   describe('getUpdatedPackages', () => {
@@ -14,5 +16,35 @@ describe('3.0', () => {
     it('should be a function', () => {
       expect(getUpdatedPackages).toBeInstanceOf(Function);
     });
+  });
+});
+
+describe('handling commands across versions', () => {
+  test('shaped commands to account for variance', () => {
+    expect(shapeLernaLsCommand()).toMatchInlineSnapshot(`"lerna ls --ndjson --since"`);
+    expect(shapeLernaUpdatedCommand()).toMatchInlineSnapshot(`"lerna updated --json"`);
+  });
+  test('handling additional and duplicate arguments', () => {
+    const duplicatedArgs = ['--since', '-a', '--since', '-a'];
+
+    expect(shapeLernaUpdatedCommand(duplicatedArgs)).toMatchInlineSnapshot(
+      `"lerna updated --json --since -a"`,
+    );
+    expect(shapeLernaLsCommand(duplicatedArgs)).toMatchInlineSnapshot(
+      `"lerna ls --ndjson --since -a"`,
+    );
+  });
+});
+
+describe('handling commands across package managers', () => {
+  beforeAll(() => {
+    process.env.USE_YARN = true;
+  });
+  afterAll(() => {
+    delete process.env.USE_YARN;
+  });
+  test('using yarn to call lerna when USE_YARN in env is true', () => {
+    expect(shapeLernaLsCommand()).toMatchInlineSnapshot(`"yarn lerna ls --ndjson --since"`);
+    expect(shapeLernaUpdatedCommand()).toMatchInlineSnapshot(`"yarn lerna updated --json"`);
   });
 });
